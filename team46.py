ecde749faf85b0daf25b7c821a4f7621876fc977
hellomasaya <harshita.runthla@gmail.com>
2019-03-01 16:52:25 +0530

    minimax started

---
+++ team46.py
@@ -0,0 +1,16 @@
+import sys
+
+class TEAM46:
+    def BestMove(self, old_move, maxDepth):
+
+		validCells = board.find_valid_move_cells(old_move)
+
+        if depth == maxDepth:
+			# return utility value
+
+		maxMove = (flag==self.who)
+
+        if maxMove:
+        
+        
+        

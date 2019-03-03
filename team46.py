28321a2a66e49e674165832c98104e921464580c
hellomasaya <harshita.runthla@gmail.com>
2019-03-04 03:42:53 +0530

    added patterns and block check

--- team46.py
+++ team46.py
@@ -5,16 +5,52 @@ class TEAM46:
         self.position_weight  = (3,2,3,2,3,2,3,2,3)
         self.boardHeuristics = {}                          
         self.blockHeuristics = {}
-        self.who = 'MaxPlayer'
+        self.who = 'MaxPlayer' #flag='x'
+        self.blockPoints = 30 #number of points if a block is won by us 
+		patterns = [] #winning patterns in block - will cotain position of flags in the winning pattern.
 
-    def board_pattern_check(self,board):
+        #vertical and horiz patterns
+        for i in xrange(3):
+			row_array = [] # i'th row
+			col_array = [] # i'th column
+			for j in xrange(3):
+				row_array.append((i,j))
+				col_array.append((j,i))
+			patterns.append(tuple(row_array))
+			patterns.append(tuple(col_array))
+
+        #diagonal patterns
+		patterns.append(((0,0) , (1,1) , (2,2)))
+		patterns.append(((0,2) , (1,1) , (2,0)))
+
+		self.patterns = tuple(patterns)
+
+    def oppFlag(self, flag):
+		# NOT operation on flag
+		return 'o' if flag == 'x' else 'x'
+
+    def board_pattern_check(self,board,flag):
         pass
     
-    def block_pattern_check(self,block):
-        pass
+    def block_pattern_check(self, block, array, flag):
+        #array - every winning pattern in patterns[]
+        flagCount = 0
+		for pos in array:
+			if block[pos[0]][pos[1]] == flag:
+				flagCount += 1
+            #1/3 will have only cell contribution written in Block_heuristics
+			elif block[pos[0]][pos[1]] == self.oppFlag(flag):
+                #if opponent's flag is present, discard
+				return 0
+		if flagCount == 2:
+			#ƒ2/3 of pattern complete. some points awarded for this
+			return somepoints
+		return 0
+        # pass
 
 
     def board_heuristics(self,board):
+        #call pattern check with every pattern in patterns[] after which give cell contribution
         #partial board = 1.125* sum if 1, 2.25*sum if 2 , for 3 game over won
         #sum is sum of block values involved in the incomplete pattern = 1/8 * 1/4 * block_heuristics * position_weight    
         pass

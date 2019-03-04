7323d7e9c55aef6e589a367f14ac974368e2725c
hellomasaya <harshita.runthla@gmail.com>
2019-03-04 08:10:38 +0530

    small-board and heuristic

--- team46.py
+++ team46.py
@@ -6,6 +6,8 @@ class TEAM46:
         self.boardHeuristics = {}                          
         self.blockHeuristics = {}
         self.max_depth = 3
+        self.boardHash = long(0)
+		self.blockHash = [[long(0) for j in xrange(3)] for i in xrange(3)]
         self.blockWin = 20 #number of points if a block is won by us 
         patterns = [] #winning patterns in block - will cotain position of flags in the winning pattern.
 
@@ -61,12 +63,22 @@ class TEAM46:
 				return 0
         
         if flagCount == 2:
-			#ƒ2/3 of pattern complete. some points awarded for this
+			#2/3 of pattern complete. some points awarded for this
 			return 6 #calculate somepoints 
 
         return 0
 
+    def block_heuristic(self, flag, block):
+		blockHeur = 0
+		for pos_arr in self.patterns:
+			blockHeur += self.block_pattern_check(block,pos_arr,flag)
+		# Finally, contribution of place (for settling tie-breakers, etc)
+		for i in xrange(3):
+			for j in xrange(3):
+				if block[i][j] == flag:
+					blockHeur += 0.1 * self.pos_weight[i][j] #!!!!!!!!!!!!!!!!!!!!!!change this
 
+		return blockHeur
     def board_heuristics(self,blockHeurs):
         # give boardHeurs for 1 block
         #partial board = 1.125* sum if 1, 2.25*sum if 2 , for 3 game over won
@@ -82,11 +94,40 @@ class TEAM46:
     def heuristics(self, flag, board):
         #call pattern check with every pattern in patterns[] after which give cell contribution in blockHeurs
         #block won = 20, block lost = -1, partial block = cell weight(1)/2*maximum cell weight(2)
-        pass    
+        # pass    
+        #hash
+        if (self.boardHash, flag) in self.boardHeuriStore:
+			return self.boardHeuriStore[(self.boardHash, flag)]
+        total = 0
 
+        small = board.small_boards_status #set block_status in 
+		b = board.big_board_status
+		blockHeurs = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+		for i in xrange(3):
+			for j in xrange(3): #add k here for two boards
+            #small is small_board
+				if small[i][j]==flag:
+					blockHeurs[i][j] = self.blockWin
+				elif small[i][j]==self.oppFlag(flag) or small[i][j]=='d':
+					blockHeurs[i][j] = -1
+        #         else:
+		# 			block = tuple([tuple(b[4*i + x][4*j:4*(j+1)]) for x in xrange(4)])
+		# 			if (self.blockHash[i][j],flag) in self.blockHeuriStore:
+		# 				blockHeurs[i][j] = self.blockHeuriStore[(self.blockHash[i][j],flag)]
+		# 			else:
+		# 				blockHeurs[i][j] = self.block_heuristic(flag,block)
+		# 				self.blockHeuriStore[(self.blockHash[i][j],flag)] = blockHeurs[i][j]
+
+		# for pos_arr in self.patterns:
+		# 	total += self.board_pattern_checker(pos_arr,blockHeurs)
+
+		# total += self.board_heuristic(blockHeurs)
+
+		# self.boardHeuriStore[(self.boardHash,flag)] = total
+		return total
 
     def Minimax(self, board, flag, depth, old_move, alpha , beta):
-        
         checkGoal = board.find_terminal_state()
         if checkGoal[1] == 'WON':
 			if checkGoal[0] == self.who:
@@ -113,7 +154,7 @@ class TEAM46:
                 bestVal = max(bestVal, self.Minimax(board,self.oppFlag(flag), depth+1, cell , alpha, beta))
                 alpha   = max( alpha, bestVal )
                 
-                board.big_boards_status[cell[0]][cell[1]] = '-'
+                board.board_status[cell[0]][cell[1]] = '-'
                 board.small_boards_status[cell[0]/3][cell[1]/3] = '-'
                 
                 if beta <= alpha:
@@ -129,7 +170,7 @@ class TEAM46:
                 bestVal = min(bestVal, self.Minimax(board,self.oppFlag(flag), depth+1, cell , alpha, beta))
                 beta    = min( beta, bestVal )
                 
-                board.big_boards_status[cell[0]][cell[1]] = '-'
+                board.board_status[cell[0]][cell[1]] = '-' 
                 board.small_boards_status[cell[0]/3][cell[1]/3] = '-'
 
                 if beta <= alpha:
@@ -140,6 +181,13 @@ class TEAM46:
     def Move(self,board, old_move, flag):
         
         self.who = flag
+
+        if old_move == (-1,-1):
+			self.addMovetoHash((4,4),1)
+			return (4,4)
+		else:
+			if board.board_status[old_move[0]][old_move[1]] == self.oppFlag(flag):
+				self.addMovetoHash(old_move,0)
 
         validCells = board.find_valid_move_cells(old_move)
         best_move = validCells[0]

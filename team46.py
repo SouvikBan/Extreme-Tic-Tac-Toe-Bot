de245579c2ad10af4eda3b1c5c69d22c079a8dd2
SouvikBan <banerjeesouvik123@gmail.com>
2019-03-04 07:33:31 +0530

    done almost

--- team46.py
+++ team46.py
@@ -2,17 +2,12 @@ import sys
 
 class TEAM46:
     def __init__(self):
-        self.position_weight  = (3,2,3,2,3,2,3,2,3)
+        self.position_weight  = ((3,2,3),(2,3,2),(3,2,3))
         self.boardHeuristics = {}                          
         self.blockHeuristics = {}
-        self.who = 'MaxPlayer' #flag='x'
-<<<<<<< HEAD
+        self.max_depth = 3
         self.blockWin = 20 #number of points if a block is won by us 
         patterns = [] #winning patterns in block - will cotain position of flags in the winning pattern.
-=======
-        self.blockPoints = 30 #number of points if a block is won by us
-		patterns = [] #winning patterns in block - will cotain position of flags in the winning pattern.
->>>>>>> 1afaf63e32489d5733034d0da14d33b44a263096
 
         #vertical and horiz patterns
         for i in xrange(3):
@@ -44,100 +39,116 @@ class TEAM46:
             patternHeur += val
             if val < 0:
                 return 0
-            elif val == self.blockPoints:
-                playerCount+=1
+            elif val == self.blockWin:
+                playerCount += 1
         
-        multiplier = 1 #when zero block won by us - included in block contribution in board_heuristics
-        if playerCount == 2: #
-            multiplier = #calculate
-        elif playerCount == 3:
-            multiplier = #big number because WON
-
-		return multiplier * patternHeur
-        # pass
+        multiplier = 1.125
+         #when zero block won by us - included in block contribution in board_heuristics
+        if playerCount == 2:
+            multiplier = 2.25
+        
+        return multiplier * patternHeur
     
     def block_pattern_check(self, block, array, flag):
         #array - every winning pattern in patterns[]
         flagCount = 0
         for pos in array:
-<<<<<<< HEAD
 			if block[pos[0]][pos[1]] == flag:
 				flagCount += 1
             #1/3 will have only cell contribution written in Block_heuristics
 			elif block[pos[0]][pos[1]] == self.oppFlag(flag):
                 #if opponent's flag is present, discard
 				return 0
+        
         if flagCount == 2:
 			#ƒ2/3 of pattern complete. some points awarded for this
-			return somepoints #calculate somepoints 
-=======
-            if block[pos[0]][pos[1]] == flag:
-                flagCount += 1
-                #1/3 will have only cell contribution written in Block_heuristics
-            elif block[pos[0]][pos[1]] == self.oppFlag(flag):
-                #if opponent's flag is present, discard
-                return 0
-        if flagCount == 2:
-            #2/3 of pattern complete. somepoints awarded for this
-            return somepoints #calculate somepoints 
->>>>>>> 1afaf63e32489d5733034d0da14d33b44a263096
+			return 6 #calculate somepoints 
+
         return 0
-        # pass
 
 
-    def board_heuristics(self,board):
+    def board_heuristics(self,blockHeurs):
         # give boardHeurs for 1 block
         #partial board = 1.125* sum if 1, 2.25*sum if 2 , for 3 game over won
         #sum is sum of block values involved in the incomplete pattern = 1/8 * 1/4 * block_heuristics * position_weight    
-        pass
+        boardHeur = 0
+        for i in xrange(3):
+			for j in xrange(3):
+				if blockHeurs[i][j] > 0:
+					boardHeur += (self.position_weight[i][j] * blockHeurs[i][j])/32
+
+        return boardHeur
     
-    def block_heuristics(self, block):
+    def heuristics(self, flag, board):
         #call pattern check with every pattern in patterns[] after which give cell contribution in blockHeurs
         #block won = 20, block lost = -1, partial block = cell weight(1)/2*maximum cell weight(2)
-        block_heuristic_value = 0
-
-        if block_pattern_check == 2:
-
-        if self.block_pattern_check(block):
-
-        #return blockHeurs
-             
-    
-    def cell_heuristics(self,index): #1/8 of weight_position
-        return (self.position_weight[index]/8)
+        pass    
 
 
+    def Minimax(self, board, flag, depth, old_move, alpha , beta):
+        
+        checkGoal = board.find_terminal_state()
+        if checkGoal[1] == 'WON':
+			if checkGoal[0] == self.who:
+				return float("inf")
+			else:
+				return float("-inf")
+        elif checkGoal[1] == 'DRAW':
+                return -100000
 
-
-
-
-    def BestMove(self, board, flag, depth, old_move, maxDepth, alpha , beta):
         validCells = board.find_valid_move_cells(old_move)
         
-        if depth == maxDepth:
-            return self.board_heuristics(board)
+        if depth == self.max_depth:
+            return self.heuristics(flag,board)
         
         maxMove = (flag == self.who)
 
         if maxMove:
             bestVal = float('-inf')
-            for child in xrange(len(validCells)):
-                bestVal = max(bestVal, self.BestMove(board,'MinPlayer',depth+1, child , maxDepth, alpha, beta))
+            for i in xrange(len(validCells)):
+
+                cell = validCells[i]
+                board.update(old_move, cell, flag)
+                
+                bestVal = max(bestVal, self.Minimax(board,self.oppFlag(flag), depth+1, cell , alpha, beta))
                 alpha   = max( alpha, bestVal )
+                
+                board.big_boards_status[cell[0]][cell[1]] = '-'
+                board.small_boards_status[cell[0]/3][cell[1]/3] = '-'
+                
                 if beta <= alpha:
                     break
                 return bestVal
         else:
             bestVal = float('inf')
-            for child in xrange(len(validCells)):
-                bestVal = min(bestVal, self.BestMove(board,'MaxPlayer',depth+1, child , maxDepth, alpha, beta))
+            for i in xrange(len(validCells)):
+                
+                cell = validCells[i]
+                board.update(old_move, cell, flag)
+                
+                bestVal = min(bestVal, self.Minimax(board,self.oppFlag(flag), depth+1, cell , alpha, beta))
                 beta    = min( beta, bestVal )
+                
+                board.big_boards_status[cell[0]][cell[1]] = '-'
+                board.small_boards_status[cell[0]/3][cell[1]/3] = '-'
+
                 if beta <= alpha:
                     break
                 return bestVal
 
    
     def Move(self,board, old_move, flag):
+        
+        self.who = flag
+
+        validCells = board.find_valid_move_cells(old_move)
+        best_move = validCells[0]
+
+        while True:
+            best_move = self.Minimax(board,'MaxPlayer', 0 , old_move, float('-inf'), float('inf'))
+            self.max_depth += 1
+        
+        return best_move
 
 
     
